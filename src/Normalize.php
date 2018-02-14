3378da8353896c15b8d3ae305a828a3e4008a3cc
Marijn Ophorst <marijn@monomelodies.nl>
2018-02-14 07:16:18 +0100

    docs and type hints

--- src/Normalize.php
+++ src/Normalize.php
@@ -4,13 +4,25 @@ namespace Monolyth\Plumber;
 
 abstract class Normalize
 {
-    public static function requestVariables()
+    /**
+     * Normalizes the GET/POST superglobals. All fields passed as empty strings
+     * (i.e., not filled by user) are set to `null`.
+     *
+     * @return void
+     */
+    public static function requestVariables() : void
     {
         array_walk($_GET, [__CLASS__, 'normalize']);
         array_walk($_POST, [__CLASS__, 'normalize']);
     }
 
-    private static function normalize(&$val)
+    /**
+     * Internal helper to recursively normalize a value.
+     *
+     * @param mixed &$val
+     * @return void
+     */
+    private static function normalize(&$val) : void
     {
         if (is_array($val)) {
             array_walk($val, [__CLASS__, 'normalize']);
